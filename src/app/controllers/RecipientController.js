f3a4df7474df3aeb4a0015127ee3eea223e07877
Yan Pantoja <yan.pantoja@liberfly.com.br>
2020-02-10 23:21:40 -0300

    added yup to validation inputs

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,28 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .required()
+        .strict(),
+      street: Yup.string().required(),
+      number: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const {
       id,
       name,
@@ -11,6 +32,47 @@ class RecipientController {
       city,
       cep,
     } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      state,
+      city,
+      cep,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().strict(),
+      street: Yup.string(),
+      number: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.body;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient not found' });
+    }
+
+    const { name, street, number, state, city, cep } = await recipient.update(
+      req.body
+    );
 
     return res.json({
       id,
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
-routes.post('/recipient', RecipientController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/recipient', RecipientController.store);
+routes.put('/recipient', RecipientController.update);
 
 export default routes;
